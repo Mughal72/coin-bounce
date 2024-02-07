653a350424cf5db296c8496d98dea5c943aea99e
Anber <ambersattarmh@gmail.com>
2024-02-07 18:19:26 +0500

    first commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,57 @@
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home/Home";
-import {BrowserRouter, Routes, Route} from 'react-router-dom'; 
-import styles from './App.module.css';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import styles from "./App.module.css";
 
 function App() {
   return (
-   <div className={styles.container}>
-    <BrowserRouter>
-    <div className={styles.layout}>
-      <Navbar />
-      <Routes>
-        <Route 
-        path="/"
-        exact
-        element = {
-        <div className={styles.main}><Home /></div>
-        }/>
-      </Routes>
-      <Footer />
+    <div className={styles.container}>
+      <BrowserRouter>
+        <div className={styles.layout}>
+          <Navbar />
+          <Routes>
+            <Route
+              path="/"
+              exact
+              element={
+                <div className={styles.main}>
+                  {" "}
+                  <Home />
+                </div>
+              }
+            />
+
+            <Route
+              path="crypto"
+              exact
+              element={<div className={styles.main}>Crypto Page</div>}
+            />
+            <Route
+              path="blogs"
+              exact
+              element={<div className={styles.main}>Blogs Page</div>}
+            />
+            <Route
+              path="submit"
+              exact
+              element={<div className={styles.main}>Submit a blog Page</div>}
+            />
+            <Route
+              path="sign-up"
+              exact
+              element={<div className={styles.main}>Sign up Page</div>}
+            />
+            <Route
+              path="log-in"
+              exact
+              element={<div className={styles.main}>Log in Page</div>}
+            />
+          </Routes>
+          <Footer />
+        </div>
+      </BrowserRouter>
     </div>
-    </BrowserRouter>
-   </div>
   );
 }
 
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,28 +1,70 @@
 import { NavLink } from "react-router-dom";
-import styles from './Navbar.module.css';
+import styles from "./Navbar.module.css";
 
-function Navbar(){
-  return(
+function Navbar() {
+  return (
     <>
-   <nav className={styles.navbar}>
-    <NavLink className={`${styles.logo} ${styles.inActiveStyle}`}
-    >CoinBounce</NavLink>
+      <nav className={styles.navbar}>
+        <NavLink to="/" className={`${styles.logo} ${styles.inActiveStyle}`}>
+          CoinBounce
+        </NavLink>
 
-    <NavLink>Home</NavLink>
-    
-    <NavLink>Crypto currencies</NavLink>
-    
-    <NavLink>Blogs</NavLink>
-    
-    <NavLink>Submit a blog</NavLink>
-    
-    <NavLink>Log In</NavLink>
-    
-    <NavLink>Sign Up</NavLink>
-   </nav>
-   <div></div>
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive ? styles.activeStyle : styles.inActiveStyle
+          }
+        >
+          Home
+        </NavLink>
+
+        <NavLink
+          to="crypto"
+          className={({ isActive }) =>
+            isActive ? styles.activeStyle : styles.inActiveStyle
+          }
+        >
+          Cryptocurrencies
+        </NavLink>
+
+        <NavLink
+          to="blogs"
+          className={({ isActive }) =>
+            isActive ? styles.activeStyle : styles.inActiveStyle
+          }
+        >
+          Blogs
+        </NavLink>
+
+        <NavLink
+          to="submit"
+          className={({ isActive }) =>
+            isActive ? styles.activeStyle : styles.inActiveStyle
+          }
+        >
+          Submit a blog
+        </NavLink>
+
+        <NavLink
+          to="log-in"
+          className={({ isActive }) =>
+            isActive ? styles.activeStyle : styles.inActiveStyle
+          }
+        >
+          <button className={styles.logInButton}>Log In</button>
+        </NavLink>
+
+        <NavLink
+          to="sign-up"
+          className={({ isActive }) =>
+            isActive ? styles.activeStyle : styles.inActiveStyle
+          }
+        >
+          <button className={styles.signUpButton}> Sign Up</button>
+        </NavLink>
+      </nav>
+      <div></div>
     </>
-    
   );
 }
 
