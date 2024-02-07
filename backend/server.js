165443dbddf49494fe55a03174be7836ff68f2cf
Anber <ambersattarmh@gmail.com>
2024-02-07 21:59:56 +0500

    first commit

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,19 @@ const {PORT} = require('./config/index');
 const router = require('./routes/index');
 const errorHandler = require('./middlewares/errorHandler');
 const cookieParser = require('cookie-parser')
+const cors = require('cors');
+
+const corsOptions = {
+    credentials: true,
+    origin: ['http//localhost:3000']
+
+}
 
 const app = express();
 
 app.use(cookieParser());
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
