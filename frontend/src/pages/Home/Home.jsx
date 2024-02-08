afb4dee0cbecdaeb747b093941e07a88b2358d81
Anber <ambersattarmh@gmail.com>
2024-02-08 17:58:54 +0500

    first commit

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,7 +1,46 @@
+import { useState, useEffect } from "react";
+import { getNews } from "../../api/external";
+import styles from "./Home.module.css";
+import Loader from "../../components/Loader/Loader";
+
 function Home() {
-    return(
-        <div> Home Page</div>
-    )
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    (async function newsApiCall() {
+      const response = await getNews();
+      setArticles(response);
+    })();
+
+    // cleanup function
+    setArticles([]);
+  }, []);
+
+  const handleCardClick = (url) => {
+    window.open(url, "_blank");
+  };
+
+  if (articles.length === 0) {
+    return <Loader text="homepage" />;
+  }
+
+  return (
+    <>
+      <div className={styles.header}>Latest Articles</div>
+      <div className={styles.grid}>
+        {articles.map((article) => (
+          <div
+            className={styles.card}
+            key={article.url}
+            onClick={() => handleCardClick(article.url)}
+          >
+           <img src={article.urlToImage} alt={article.title} />
+            <h3>{article.title}</h3>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default Home;
